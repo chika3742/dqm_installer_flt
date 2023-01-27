f926eedbd9253bc3b2680b7463a699eed64a9f2f
chika3742 <kazu.chika.shima@gmail.com>
2023-01-27 17:52:24 +0900

    macOSの不要な実装の削除

--- lib/utils/bundles.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dqm_installer_flt/api/utils_api.dart';
-
-Future<String> get7zPath() async {
-  var bundlePath = await UtilsApi().getBundlePath("7zz");
-  if (bundlePath == null) {
-    throw Exception("Failed to get bundle path");
-  }
-  return bundlePath;
-}
